b543c53fc33d11c85c1930f388c657282d19daff
Yash Patel <yashpatel200403@gmail.com>
2023-01-14 22:33:40 +0530

    Dutch auction smartContract half done

--- DutchAuction.sol
+++ DutchAuction.sol
@@ -1,7 +1,19 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.13;
 
 contract DutchAuction {
     
+    //  Variable for starting and ending auction
+    address payable public immutable seller;
+    uint public immutable startingPrice;
+    uint public immutable startingTime;
+    uint public immutable endingTime;
+
+    // Discount rate at which the price decreases
+    uint public immutable discountRate;
+
+
+    // Event for selling
+    event sold(address buyer, uint price);
 }
