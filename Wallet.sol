ee04fe547c705c98d1d5a23ff0a1d1c6cab9c621
Yash Patel <yashpatel200403@gmail.com>
2023-01-27 20:57:22 +0530

    Adding simple wallet contract not completed

--- Wallet.sol
+++ Wallet.sol
@@ -7,5 +7,22 @@ contract SimpleWallet {
     //  Address of the wallet
     address public wallet;
 
-    // 
+    // function to initiliaze wallet
+    function initialize() public {
+        wallet = address(this);
+    }
+
+    // function to send Ether from wallet
+    function sendEther(address _to, uint _value) public{
+        require(msg.sender == wallet,"Only the wallet can send Ether.");
+        _to.transfer(_value);
+    }
+
+    function receiveEther() public payable {
+        require(msg.sender == wallet,"ONly wallet can receive ether");
+    }
+
+    function getBalance() public view returns(uint){
+        return address(this).balance;
+    }
 }
